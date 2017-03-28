4f16dc4b900c00f004054056f48bacba79d1310f
Cody Fuller <mail@codyfuller.com>
2017-03-27 23:49:53 -0500

    Add auth_url to user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :hello, :auth_token
+  attributes :id, :email, :auth_url
 
   def hello
     "world"
+  end
+
+  def auth_url
+    client_id =  ENV["SPOTIFY_CLIENT_ID"]
+    callback_url = Rack::Utils.escape("http://localhost:3000/auth/spotify/callback")
+    scope = 'user-read-email playlist-modify-public user-library-read playlist-read-private playlist-read-collaborative user-top-read'.tr(' ', '+')
+    state = object.auth_token
+    auth_uri = "https://accounts.spotify.com/authorize?client_id=#{client_id}&redirect_uri=#{callback_url}&response_type=code&scope=#{scope}&state=#{state}"
+    auth_uri
   end
 end
