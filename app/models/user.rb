4c2b10bcb4537f3974c05ebcfbc444d63f3abcdc
Cody Fuller <mail@codyfuller.com>
2017-03-27 23:51:12 -0500

    Fix copy-paste error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   serialize :spotify_hash
 
-  has_many :subscriptions, class_name: 'Subscribe'
+  has_many :subscriptions
   has_many :playlists, through: :subscriptions
 
   has_secure_password
