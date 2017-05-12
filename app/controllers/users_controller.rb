ce6b809db4d5b46007d0d30cb4767e01792db7db
Cody Fuller <git@codyfuller.com>
2017-05-12 15:07:32 -0500

    Move callback host to ENV variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
   # https://accounts.spotify.com/authorize?client_id=5c97b616cf644c148bc35b47ffdf2cab&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fspotify%2Fcallback&response_type=code&scope=user-read-email+playlist-modify-public+user-library-read+playlist-read-private+playlist-read-collaborative+user-top-read&state=e9aaeb7c32b97303e465f0f4571a7bbe
 
   def spotify_auth
-    client_id =  ENV["SPOTIFY_CLIENT_ID"]
-    callback_url = Rack::Utils.escape("http://localhost:3000/auth/spotify/callback")
+    client_id =  ENV['SPOTIFY_CLIENT_ID']
+    callback_url = Rack::Utils.escape("#{ENV['CALLBACK_HOST']}/auth/spotify/callback")
     scope = 'user-read-email playlist-modify-public user-library-read playlist-read-private playlist-read-collaborative user-top-read'.tr(' ', '+')
     state = @current_user.auth_token
     auth_url = "https://accounts.spotify.com/authorize?client_id=#{client_id}&redirect_uri=#{callback_url}&response_type=code&scope=#{scope}&state=#{state}"
     redirect_to auth_url
   end
-  
+
   def spotify_callback
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     user = User.find_by(auth_token: params['state'])
